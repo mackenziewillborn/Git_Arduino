285aca90432a3a77d6d98e02152000e38e5df057
Mackenzie Willborn <mlw60@duke.edu>
2019-01-30 12:50:49 -0500

    sample test for single side of scale w/ original calibration number

---
+++ ScaleSampleTest.ino
@@ -0,0 +1,30 @@
+// see Scale Calibration to attain calibration factor 
+// This example code uses bogde's excellent library:  https://github.com/bogde/HX711
+//
+// bogde's library is released under a GNU GENERAL PUBLIC LICENSE
+// The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
+// based load cell which should allow a user to measure everything from a few grams to tens of tons.
+// Arduino pin 2 -> HX711 CLK
+// 3 -> DAT
+// 5V -> VCC
+// GND -> GND
+// The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+
+#include "HX711.h"
+#define calibration_factor -11280.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define DOUT  3
+#define CLK  2
+HX711 scale(DOUT, CLK);
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 scale demo");
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+  Serial.println("Readings:");
+}
+void loop() {
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
+  Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
+  Serial.println();
+}
